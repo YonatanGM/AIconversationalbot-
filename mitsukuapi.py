8b3e5b6852b8313686d1651594d4685a5004dde0
Yonatan Mamo <girmayonatan86@gmail.com>
2019-09-06 19:23:21 +0300

    Update mitsukuapi.py

--- mitsukuapi.py
+++ mitsukuapi.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-# Distributed under the MIT License.
-
-"""An API for Mitsuku[https://www.pandorabots.com/mitsuku/], a chatbot.
-
-This is _not_ official.  It's just a little project that I'm working
-on.  Usage:
-```sh
-python3 main.py [file]
-```
-file: input to chatbot.  If not found, you will enter it in stdin.
-
-As for now, it just outputs the response to stdout.
-
-You need the following stuff for it to work:
-- Any distribution of Python, version 3.6 or above
-- beautifulsoup4
-- lxml
-- requests
-- Internet access
-"""
-
 import json
 import random
 import re
